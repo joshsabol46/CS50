2a0a63d73460ac07718e0ad20b5a6e7f97c85f6a
Josh Sabol <joshua.l.sabol@gmail.com>
2020-09-26 17:10:31 -0700

    done

--- wiki3/encyclopedia/urls.py
+++ wiki3/encyclopedia/urls.py
@@ -5,10 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
-    path("edit_page/<str:title>", views.edit_page, name="edit_page"),
-    path("random_page", views.random_page, name="random_page"),
     path("search", views.search, name="search"),
-    path("add_page", views.add_page, name="add_page"),
-
+    path("add", views.add, name="add"),
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("random_page", views.random_page, name="random_page"),
 
 ]
--- wiki3/encyclopedia/views.py
+++ wiki3/encyclopedia/views.py
@@ -2,26 +2,19 @@ import markdown2
 import random
 import re
 from django.shortcuts import render, redirect, HttpResponse
+from django.http import HttpResponseRedirect
 
 from . import util
-
-pagelist = util.list_entries()
-
-# default page
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-# rendering page content for a title
-
-
 def wiki(request, title):
-    pagelist = util.list_entries()
+    entries = util.list_entries()
 
-    if title in pagelist:
+    if title in entries:
         content = util.get_entry(title)
 
         return render(request, "encyclopedia/page.html", {
@@ -34,49 +27,60 @@ def wiki(request, title):
             'error_message': 'Page not found'
         })
 
-# random page
-
-
-def random_page(request):
-
-    # random no. in [0-len[pagelist]-1]]
-    r = random.randint(0, len(pagelist)-1)
-    title = pagelist[r]
-
-    return redirect(wiki, title=title)  # redirect to this page
-
-# search
-
+# Search #1
+# Breaks when searching for lower exact match
 
 def search(request):
-    if request.method == 'POST':
-        term = request.POST
-        term = term['q']
-        searchlist = []
-
-        for page in pagelist:
-
-            if re.search(term.lower(), page.lower()):  # case in sensitive
-                searchlist.append(page)
-        if len(searchlist) == 0:  # searchlist is empty
+    entries = util.list_entries()
+    query = request.POST.get("q")
+    if util.get_entry(query):
+        return redirect("wiki", title=query)
+    else:
+        match = []
+        for i in entries:
+            if query.lower() in i.lower():
+                match.append(i)
+        if len(match) == 0:
             return render(request, "encyclopedia/error.html", {
-                'error_message': f'No results found for \'{term}\' '
+                'error_message': f'No results found for \'{query}\' '
+            })
+        else:
+            return render(request, 'encyclopedia/search.html', {
+                "results" : match,
+                "search" : query
             })
 
-    return render(request, "encyclopedia/search.html", {
-        'entries': searchlist
-    })
+# Search #2
+# def search(request):
+#     if request.method =='POST':
+#         response = request.POST["q"]
+#         entry = util.get_entry(response)
+#         if entry:
+#             print(entry)
+#             return HttpResponseRedirect("wiki/"+entry)
+#         else:
+#             entries = util.list_entries()
+#             search_entries = [i for i in entries if response.lower() in i.lower()]
+#             if search_entries:
+#                 return render(request, 'encyclopedia/index.html',{  
+#                     "entries": search_entries
+#                 })
+#             else:
+#                 return render(request, "encyclopedia/error.html",{
+#                     "error": "The page you are looking for does not exist"
+#                 })            
 
-# new page
 
+def add(request):
 
-def add_page(request):
+    entries = util.list_entries()
+
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
         print(f'title:{title}')
         print(f'content:{content}')
-        if title in pagelist:
+        if title in entries:
             return render(request, "encyclopedia/add.html", {
                 'available': True
                 # page already exists
@@ -89,10 +93,7 @@ def add_page(request):
 
     })
 
-# edit markdown
-
-
-def edit_page(request, title):
+def edit(request, title):
     pagecontent = util.get_entry(title)
     if request.method == 'GET':
         return render(request, "encyclopedia/edit.html", {
@@ -103,3 +104,10 @@ def edit_page(request, title):
         pagecontent = request.POST.get('newcontent')
         util.save_entry(title, pagecontent)
         return redirect(wiki, title=title)
+
+
+def random_page(request):
+    entries = util.list_entries()
+
+    # redirect to a page where the title is a random value in an array of entries
+    return redirect(wiki, title=entries[random.randint(0, len(entries)-1)])  
